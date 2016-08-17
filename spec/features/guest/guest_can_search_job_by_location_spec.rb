051b3fc6e5c784ac952284334b76d2030127adcc
Nicole Marino <nicolekmarino@hotmail.com>
2016-08-17 15:32:26 -0600

    added test for location search

--- spec/features/guest/guest_can_search_job_by_location_spec.rb
+++ spec/features/guest/guest_can_search_job_by_location_spec.rb
@@ -2,25 +2,23 @@ require 'rails_helper'
 require "support/test_helper"
 
 RSpec.feature "Guest can search jobs by location" do
-  scenario "guest visits companies page and filters job list by location" do
-	
-  create_three_jobs
-  visit root_path
-  click_on "Search Jobs"
-  
-  expect(current_path).to eq(jobs_path)
-  
-  expect(page).to have_content("Filter By")
+  scenario "guest visits companies page and filters job list by location"  do
 
-  expect(page).to have_content("Software Integration Developer")
-  expect(page).to have_content("Senior Software Integration Developer")
-  expect(page).to have_content("Designer")
+    create_three_jobs
+    visit root_path
+    click_on "Search Jobs"
 
-  select('CO', from: 'job[filter_location]') #not working
+    expect(current_path).to eq(jobs_path)
 
-  expect(page).to have_content("Software Integration Developer")
-  expect(page).to have_content("Senior Software Integration Developer")
-  # expect(page).to_not have_content("Designer")
+    expect(page).to have_content("Filter By")
+    expect(page).to have_content("Software Integration Developer")
+    expect(page).to have_content("Senior Software Integration Developer")
+    expect(page).to have_content("Designer")
 
+    select('CO', from: 'job[filter_location]')
+
+    expect(page).to have_content("Software Integration Developer")
+    expect(page).to have_content("Senior Software Integration Developer")
+    expect(page).to have_selector('.FL', visible: false)
   end
 end
