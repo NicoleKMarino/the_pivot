f6c08153d5ff5af67f83e645ffc5098820eea6f4
Lane Winham <mlwinham12@gmail.com>
2016-08-17 16:44:09 -0600

    Add salary search filter

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,25 @@
 class JobsController < ApplicationController
   def index
     @jobs = Job.where(status: 0)
-    @locations = @jobs.map { |job| job.company.location }.uniq
+    @locations = location_list
+    @salaries = salary_list
   end
 
   def unavailable
-    @jobs = Job.where(status: 1)
+    @jobs_unavailable = Job.where(status: 1)
   end
 
   def show
     @jobs = Job.find(params[:id])
   end
-  
+
+  private
+
+  def salary_list
+    @jobs.map { |job| job.salary }.uniq
+  end
+
+  def location_list
+    @jobs.map { |job| job.company.location }.uniq
+  end
 end
--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,8 +1,8 @@
 def create_company
   industry = Industry.create!(name: "Tech")
   industry.companies.create!(
-    name: "Inspirato", 
-    description: "Great company", 
+    name: "Inspirato",
+    description: "Great company",
     location: "CO",
     status: 2,
     img_path: "http://kpcbweb2.s3.amazonaws.com/companies/422/logo/original/Inspirato_logotype_K_-%C2%AB.jpg?1339101904"
@@ -13,16 +13,16 @@ def create_three_companies
   create_company
   industry2 = Industry.create!(name: "Advertising")
   industry2.companies.create!(
-    name: "Integer", 
-    description: "Nice company", 
+    name: "Integer",
+    description: "Nice company",
     location: "CO",
     status: 2,
     img_path: "http://kpcbweb2.s3.amazonaws.com/companies/422/logo/original/Inspirato_logotype_K_-%C2%AB.jpg?1339101904"
   )
   industry3 = Industry.create!(name: "Finance")
   industry3.companies.create!(
-    name: "TD Ameritrade", 
-    description: "Rich company", 
+    name: "TD Ameritrade",
+    description: "Rich company",
     location: "FL",
     status: 2,
     img_path: "http://kpcbweb2.s3.amazonaws.com/companies/422/logo/original/Inspirato_logotype_K_-%C2%AB.jpg?1339101904"
