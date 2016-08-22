cff155bff79af22bfb4a54699b6da053aaead224
Deborah Hamel <deborahleehamel@gmail.com>
2016-08-22 15:41:43 -0600

    hound updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,11 @@
 module ApplicationHelper
-
   def admin_edit?
     platform_admin?
   end
 
-
   def new_user_path?
     request.path == new_user_path || formatted_referrer == new_user_path
   end
-
 
   def platform_admin?
     current_user && current_user.platform_admin?
@@ -20,8 +17,7 @@ module ApplicationHelper
 
   private
 
-  def formatted_referrer
-    '/' + request.referrer.split('/')[-2..-1].join('/') if request.referrer
-  end
-
+    def formatted_referrer
+      '/' + request.referrer.split('/')[-2..-1].join('/') if request.referrer
+    end
 end
