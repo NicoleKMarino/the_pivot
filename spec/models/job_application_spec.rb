4002b33715ad03aa455602d70e4399b350728e27
Roberta Doyle <ro.scalabrin@gmail.com>
2016-08-23 08:08:15 -0600

    fix hound issues

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe JobApplication, type: :model do
     job_application.assign_user(user)
     expect(job_application.user_id).to eq(user.id)
   end
-  
+
   it "can change the status to submitted" do
     job_application = create_job_application
     job_application.change_status_to_submitted
