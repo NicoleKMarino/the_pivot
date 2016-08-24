4bf33500b81481febee5e51e0bbe025f0e3acf56
Deborah Hamel <deborahleehamel@gmail.com>
2016-08-24 11:07:04 -0600

    updates for hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ class Seed
         city: "Denver",
         state: "CO",
         zip_code: 80215
-        )
+      )
     end
     registered_user_roles
   end
@@ -44,17 +44,17 @@ class Seed
   def self.employer_user_roles
     20.times do |i|
       UserRole.create!(
-      user_id: 100 + i + 1,
-      role_id: 2
+        user_id: 100 + i + 1,
+        role_id: 2
       )
     end
   end
 
   def self.platform_admin_user_roles
-      UserRole.create!(
+    UserRole.create!(
       user_id: 121,
       role_id: 3
-      )
+    )
   end
 
   def self.create_employers
@@ -69,7 +69,7 @@ class Seed
         city: "Denver",
         state: "CO",
         zip_code: 80215
-        )
+      )
     end
     employer_user_roles
   end
@@ -85,44 +85,45 @@ class Seed
       city: "Denver",
       state: "CO",
       zip_code: 80215
-      )
+    )
     platform_admin_user_roles
   end
 
   def self.create_industries
-   Industry.create!(name: "Tech")
-   Industry.create!(name: "Advertising")
-   Industry.create!(name: 'Finance')
-   Industry.create!(name: 'Education')
-   Industry.create!(name: 'Healthcare')
-   Industry.create!(name: 'Government')
-   Industry.create!(name: 'Law')
-   Industry.create!(name: 'Entertainment')
-   Industry.create!(name: 'Construction')
-   Industry.create!(name: 'Hospitality')
+    Industry.create!(name: "Tech")
+    Industry.create!(name: "Advertising")
+    Industry.create!(name: 'Finance')
+    Industry.create!(name: 'Education')
+    Industry.create!(name: 'Healthcare')
+    Industry.create!(name: 'Government')
+    Industry.create!(name: 'Law')
+    Industry.create!(name: 'Entertainment')
+    Industry.create!(name: 'Construction')
+    Industry.create!(name: 'Hospitality')
   end
 
   def self.create_companies_status_online
-   20.times do |i|
-    user = rand(66..121)
-    industry = Industry.offset(rand(Industry.count)).first
-    industry.companies.create!(
-      name: Faker::Company.name,
-      description: Faker::Company.catch_phrase,
-      location: Faker::Address.state,
-      img_path: Faker::Company.logo,
-      user_id: user)
+    20.times do |i|
+      user = rand(66..121)
+      industry = Industry.offset(rand(Industry.count)).first
+      industry.companies.create!(
+        name: Faker::Company.name,
+        description: Faker::Company.catch_phrase,
+        location: Faker::Address.state,
+        img_path: Faker::Company.logo,
+        user_id: user
+      )
    end
   end
 
   def self.generate_jobs
     Industry.all.each do |industry|
       industry.companies.each do |company|
-      50.times do |i|
-       company.jobs.create!(
-        title: Faker::Company.profession,
-        description: Faker::Lorem.paragraph(2),
-        salary: ["$40,000-$60,000", "$60,000-$80,000", "$80,000-$100,000"].sample)
+        50.times do |i|
+          company.jobs.create!(
+            title: Faker::Company.profession,
+            description: Faker::Lorem.paragraph(2),
+            salary: ["$40,000-$60,000", "$60,000-$80,000", "$80,000-$100,000"].sample)
         end
       end
     end
