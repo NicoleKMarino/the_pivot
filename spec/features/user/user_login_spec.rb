52b878c4f5e425e4e4548baf5918e57cb7b310eb
Roberta Doyle <ro.scalabrin@gmail.com>
2016-08-25 00:59:37 -0600

    add test for login sad path

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "User can login" do
-  scenario "registered user can login" do
+  
+  before do
     role = Role.create(name: "registered_user")
     role.users.create!(username: "lane", email: "lane@nil.com", password: "password")
-    
+  end
+  
+  scenario "registered user can login with valid credentials" do  
     visit login_path
 
     fill_in "Username", with: "lane"
@@ -16,4 +19,16 @@ RSpec.feature "User can login" do
 
     expect(page).not_to have_content "Login"
   end
+  
+  scenario "registered user cannot login with invalid credentials" do
+    visit login_path
+
+    fill_in "Username", with: "lane"
+    fill_in "Password", with: "password2"
+    click_button "Login"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content "Login information incorrect."
+  end
+
 end
